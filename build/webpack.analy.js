2f980d4e8971738d4dc1cae38df2b247d9c511e4
zhongzhisen <zhongzhisen@qunjielong.com>
2023-02-07 17:14:24 +0800

    feat: 添加webpack-bundle-analyzer插件，分析打包情况

--- build/webpack.analy.js
+++ build/webpack.analy.js
@@ -2,10 +2,14 @@ const prodConfig = require("./webpack.prod")
 const SpeedMeasureWebpackPlugin = require("speed-measure-webpack-plugin")
 const smp = new SpeedMeasureWebpackPlugin()
 const { merge } = require("webpack-merge")
+const { BundleAnalyzerPlugin  } = require("webpack-bundle-analyzer")
 
 // 使用smp.wrap将生产配置传出去
 module.exports = smp.wrap(
   merge(prodConfig, {
-
+    plugins: [
+      // 配置分析打包结果插件
+      new BundleAnalyzerPlugin ()
+    ]
   })
 )
