3f949fb118f6ac65f05caca0cad283aac8318195
zhongzhisen <zhongzhisen@qunjielong.com>
2023-02-07 16:08:43 +0800

    feat: 复制public下的静态资源到dist文件夹下

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,7 +1,23 @@
 const { merge } = require("webpack-merge")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 const baseConfig = require("./webpack.base")
+const path = require("path")
 
 module.exports = merge(baseConfig, {
   // 开发模式，打包更快，省掉优化代码
   mode: "production",
+  plugins: [
+    // 复制文件插件
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "../public"),
+          to: path.resolve(__dirname, "../dist"),
+          filter: source => {
+            return !source.includes("index.html")
+          }
+        }
+      ]
+    })
+  ]
 })
